578817f0ed688c0c5079598ce48203a8309c1455
blueboomer76 <39141684+blueboomer76@users.noreply.github.com>
2019-07-18 13:46:05 -0700

    Change background generator into object with a function

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Pattern Generator
 A pattern generator for creating backgrounds for HTML pages, made with JavaScript. Many different patterns and gradients are possible, and random ones can be generated along with the text colors.
 
-To use, include the `patternGenerator.js` file directly in your projects to create random backgrounds.
+Include the `patternGenerator.js` file directly in your projects, then add some code like the following to your HTML:
+
+```html
+<script src="patternGenerator.js"></script>
+<script>patternGenerator.applyRandomPattern();</script>
+```
--- patternGenerator.js
+++ patternGenerator.js
@@ -1,55 +1,59 @@
-let colors = [];
-if (Math.random() < 0.5) {
-	if (Math.random() < 0.5) {
-		for (let i = 0; i < 6; i++) colors.push(Math.floor(Math.random() * 256));
+const patternGenerator = {
+	applyRandomPattern: () => {
+		let colors = [];
+		if (Math.random() < 0.5) {
+			if (Math.random() < 0.5) {
+				for (let i = 0; i < 6; i++) colors.push(Math.floor(Math.random() * 256));
 
-		let direction = `${Math.floor(Math.random() * 360)}deg`;
-		let color1 = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
-		let color2 = `rgb(${colors[3]}, ${colors[4]}, ${colors[5]})`;
-		let colorStop = `${(Math.random() * 30 + 35).toFixed(1)}%`;
+				let direction = `${Math.floor(Math.random() * 360)}deg`;
+				let color1 = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
+				let color2 = `rgb(${colors[3]}, ${colors[4]}, ${colors[5]})`;
+				let colorStop = `${(Math.random() * 30 + 35).toFixed(1)}%`;
 
-		document.body.style.background = `linear-gradient(${direction}, ${color1}, ${color2} ${colorStop}) fixed`;
-	} else {
-		for (let i = 0; i < 9; i++) colors.push(Math.floor(Math.random() * 256));
+				document.body.style.background = `linear-gradient(${direction}, ${color1}, ${color2} ${colorStop}) fixed`;
+			} else {
+				for (let i = 0; i < 9; i++) colors.push(Math.floor(Math.random() * 256));
 
-		let direction = `${Math.floor(Math.random() * 360)}deg`;
-		let color1 = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
-		let color2 = `rgb(${colors[3]}, ${colors[4]}, ${colors[5]})`;
-		let color3 = `rgb(${colors[6]}, ${colors[7]}, ${colors[8]})`;
-		let colorStop1 = `${(Math.random() * 10).toFixed(1)}%`;
-		let colorStop2 = `${(Math.random() * 20 + 40).toFixed(1)}%`;
-		let colorStop3 = `${(Math.random() * 10 + 90).toFixed(1)}%`;
+				let direction = `${Math.floor(Math.random() * 360)}deg`;
+				let color1 = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
+				let color2 = `rgb(${colors[3]}, ${colors[4]}, ${colors[5]})`;
+				let color3 = `rgb(${colors[6]}, ${colors[7]}, ${colors[8]})`;
+				let colorStop1 = `${(Math.random() * 10).toFixed(1)}%`;
+				let colorStop2 = `${(Math.random() * 20 + 40).toFixed(1)}%`;
+				let colorStop3 = `${(Math.random() * 10 + 90).toFixed(1)}%`;
 
-		document.body.style.background = `linear-gradient(${direction}, ${color1} ${colorStop1}, ${color2} ${colorStop2}, ${color3} ${colorStop3}) fixed`;
-	}
-} else {
-	let center = `${(Math.random() * 100).toFixed(1)}% ${(Math.random() * 100).toFixed(1)}%`;
-	if (Math.random() < 0.5) {
-		for (let i = 0; i < 6; i++) {colors.push(Math.floor(Math.random() * 256));};
+				document.body.style.background = `linear-gradient(${direction}, ${color1} ${colorStop1}, ${color2} ${colorStop2}, ${color3} ${colorStop3}) fixed`;
+			}
+		} else {
+			let center = `${(Math.random() * 100).toFixed(1)}% ${(Math.random() * 100).toFixed(1)}%`;
+			if (Math.random() < 0.5) {
+				for (let i = 0; i < 6; i++) {colors.push(Math.floor(Math.random() * 256));};
 
-		let color1 = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
-		let color2 = `rgb(${colors[3]}, ${colors[4]}, ${colors[5]})`;
-		let colorStop = `${(Math.random() * 30 + 35).toFixed(1)}%`;
+				let color1 = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
+				let color2 = `rgb(${colors[3]}, ${colors[4]}, ${colors[5]})`;
+				let colorStop = `${(Math.random() * 30 + 35).toFixed(1)}%`;
 
-		document.body.style.background = `radial-gradient(circle at ${center}, ${color1}, ${color2} ${colorStop}) fixed`;
-	} else {
-		for (let i = 0; i < 9; i++) colors.push(Math.floor(Math.random() * 256));
+				document.body.style.background = `radial-gradient(circle at ${center}, ${color1}, ${color2} ${colorStop}) fixed`;
+			} else {
+				for (let i = 0; i < 9; i++) colors.push(Math.floor(Math.random() * 256));
 
-		let color1 = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
-		let color2 = `rgb(${colors[3]}, ${colors[4]}, ${colors[5]})`;
-		let color3 = `rgb(${colors[6]}, ${colors[7]}, ${colors[8]})`;
-		let colorStop1 = `${(Math.random() * 10).toFixed(1)}%`;
-		let colorStop2 = `${(Math.random() * 20 + 40).toFixed(1)}%`;
-		let colorStop3 = `${(Math.random() * 10 + 90).toFixed(1)}%`;
+				let color1 = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
+				let color2 = `rgb(${colors[3]}, ${colors[4]}, ${colors[5]})`;
+				let color3 = `rgb(${colors[6]}, ${colors[7]}, ${colors[8]})`;
+				let colorStop1 = `${(Math.random() * 10).toFixed(1)}%`;
+				let colorStop2 = `${(Math.random() * 20 + 40).toFixed(1)}%`;
+				let colorStop3 = `${(Math.random() * 10 + 90).toFixed(1)}%`;
 
-		document.body.style.background = `radial-gradient(circle at ${center}, ${color1} ${colorStop1}, ${color2} ${colorStop2}, ${color3} ${colorStop3}) fixed`;
+				document.body.style.background = `radial-gradient(circle at ${center}, ${color1} ${colorStop1}, ${color2} ${colorStop2}, ${color3} ${colorStop3}) fixed`;
+			}
+		}
+
+		let textColors = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
+		if (textColors[0] + textColors[1] + textColors[2] < 384) {
+			document.body.style.textShadow = "1px 1px 1px rgb(255, 255, 255), 1px 1px 1px rgb(255, 255, 255)";
+		} else {
+			document.body.style.textShadow = "1px 1px 1px rgb(0, 0, 0), 1px 1px 1px rgb(0, 0, 0)";
+		}
+		document.body.style.color = `rgb(${textColors[0]}, ${textColors[1]}, ${textColors[2]})`;
 	}
 }
-
-let textColors = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
-if (textColors[0] + textColors[1] + textColors[2] < 384) {
-	document.body.style.textShadow = "1px 1px 1px rgb(255, 255, 255), 1px 1px 1px rgb(255, 255, 255)";
-} else {
-	document.body.style.textShadow = "1px 1px 1px rgb(0, 0, 0), 1px 1px 1px rgb(0, 0, 0)";
-}
-document.body.style.color = `rgb(${textColors[0]}, ${textColors[1]}, ${textColors[2]})`;
